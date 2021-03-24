74eda1a2148fba8565e85000559681927bb910e4
ishan chaudhary <ishan4220@gmail.com>
2021-03-24 14:42:30 +0530

    Added new photos

--- src/components/Home/GridImages/GridImages.js
+++ src/components/Home/GridImages/GridImages.js
@@ -1,28 +1,62 @@
 import React from "react";
 import "./GridImages.css";
-import poster from "../../../images/black-panther-poster.jpg";
-import actor from "../../../images/saif-ali-khan.jpg";
-import actor2 from "../../../images/actor.jpg"
-import tvShow from "../../../images/tvshow-poster.jpg";
-import actress from "../../../images/actress.jpg";
-import webSeries from "../../../images/webseries-poster.jpg";
-import tvActor from "../../../images/tv-actor.jpg";
-// SECOND
-import poster1 from "../../../images/black-panther-poster-1.jpg";
-import actor1 from "../../../images/actor-1.jpg";
-import tvShow1 from "../../../images/tvshow-poster-1.jpg";
-import actress1 from "../../../images/actress-1.jpg";
-import webSeries1 from "../../../images/webseries-poster-1.jpg";
-import tvActor1 from "../../../images/tv-actor-1.jpg";
 import $ from "jquery";
+
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images
+}
+
+const bigImages = importAll(require.context('../../../images/gridLong', false, /\.(png|jpe?g|svg)$/));
+const smallImages = importAll(require.context('../../../images/gridLong', false, /\.(png|jpe?g|svg)$/));
+
+let x1 = [];
+let y1 = [];
+let x2 = [];
+let y2 = [];
+
+let bigKeys = Object.keys(bigImages);
+let smallKeys = Object.keys(smallImages);
+
+function chunkArray(arr,n){
+  var chunkLength = Math.max(arr.length/n ,1);
+  var chunks = [];
+  for (var i = 0; i < n; i++) {
+      if(chunkLength*(i+1)<=arr.length)chunks.push(arr.slice(chunkLength*i, chunkLength*(i+1)));
+  }
+  return chunks; 
+}
+
+let partedArray = chunkArray(bigKeys,2);
+let bigKeys1 = partedArray[0];
+let bigKeys2 = partedArray[1];
+partedArray = chunkArray(smallKeys,2);
+let smallKeys1 = partedArray[0];
+let smallKeys2 = partedArray[1];
+
+let it1 = 0;
+let it2 = 0;
+
+for (let n = 0; n < 10; n++) {
+  if (n === 0) {
+    x1.push(smallImages[smallKeys1[it1++]].default, bigImages[bigKeys1[it1++]].default, bigImages[bigKeys1[it1++]].default, smallImages[smallKeys1[it1++]].default, smallImages[smallKeys1[it1++]].default, bigImages[bigKeys1[it1++]].default);
+    x2.push(smallImages[smallKeys2[it2++]].default, bigImages[bigKeys2[it2++]].default, bigImages[bigKeys2[it2++]].default, smallImages[smallKeys2[it2++]].default, smallImages[smallKeys2[it2++]].default, bigImages[bigKeys2[it2++]].default);
+  } else {
+    y1.push(smallImages[smallKeys1[it1++]].default, bigImages[bigKeys1[it1++]].default, bigImages[bigKeys1[it1++]].default, smallImages[smallKeys1[it1++]].default, smallImages[smallKeys1[it1++]].default, bigImages[bigKeys1[it1++]].default);
+    y2.push(smallImages[smallKeys2[it2++]].default, bigImages[bigKeys2[it2++]].default, bigImages[bigKeys2[it2++]].default, smallImages[smallKeys2[it2++]].default, smallImages[smallKeys2[it2++]].default, bigImages[bigKeys2[it2++]].default);
+  }
+}
 
 const GridImages = ({ secId }) => {
   let count = -1;
-  let newImg = x.length; //6
+
+  let newImg1 = x1.length;
+  let newImg2 = x2.length;
 
   const fadeOutt = () => {
     let interval = setInterval(function () {
-      count >= x.length - 1 ? (count = 0) : count++;
+      count >= x1.length - 1 ? (count = 0) : count++;
 
       $(`#${count}`).fadeOut(800);
       $(`#secGrid-${count}`).fadeOut(800);
@@ -35,12 +69,14 @@ const GridImages = ({ secId }) => {
   const fadeIn = () => {
     let interval = setInterval(function () {
       $(`#${count}`).css({
-        background: `url('${y[newImg]}') no-repeat center center`,
+        background: `url('${y1[newImg1]}') no-repeat center center`,
       });
       $(`#secGrid-${count}`).css({
-        background: `url('${y[newImg]}') no-repeat center center`,
+        background: `url('${y2[newImg2]}') no-repeat center center`,
       });
-      newImg >= y.length - 1 ? (newImg = 0) : newImg++;
+
+      newImg1 >= y1.length - 1 ? (newImg1 = 0) : newImg1++;
+      newImg2 >= y2.length - 1 ? (newImg2 = 0) : newImg2++;
 
       $(`#${count}`).fadeIn(400);
       $(`#secGrid-${count}`).fadeIn(400);
@@ -54,11 +90,21 @@ const GridImages = ({ secId }) => {
     fadeOutt();
     return (
       <div className="gridImages">
-        {x.map((image, index) => (
+        {secId ? x2.map((image, index) => (
           <div
             key={index}
             id={`${secId ? `secGrid-${index}` : `${index}`}`}
-            className={`gridImagesBox gridImages__${index} fadeInOut-${index}`}
+            className={`gridImagesBox gridImages__${index} `}
+            style={{
+              background: `url('${image}') no-repeat center center contain`,
+              // backgroundSize: "cover",
+            }}
+          ></div>
+        )) : x1.map((image, index) => (
+          <div
+            key={index}
+            id={`${secId ? `secGrid-${index}` : `${index}`}`}
+            className={`gridImagesBox gridImages__${index} `}
             style={{
               background: `url('${image}') no-repeat center center contain`,
               // backgroundSize: "cover",
@@ -71,27 +117,5 @@ const GridImages = ({ secId }) => {
 
   return renderImage();
 };
-
-const x = [poster, actress, actor, webSeries, tvShow, tvActor];
-const y = [
-  poster,
-  actress,
-  actor,
-  webSeries,
-  tvShow,
-  tvActor,
-  poster1,
-  actress1,
-  actor1,
-  webSeries1,
-  tvShow1,
-  tvActor1,
-  poster1,
-  actress1,
-  actor2,
-  webSeries1,
-  tvShow1,
-  tvActor1,
-];
 
 export default GridImages;
